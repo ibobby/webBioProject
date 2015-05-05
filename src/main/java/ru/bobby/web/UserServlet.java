f7cfd5552cdfff9b03a35fb1d19e62a56cec70bb
bobby <ibobby0386@gmail.com>
2015-05-05 17:43:26 +0300

    add first servlets and logging

---
+++ src/main/java/ru/bobby/web/UserServlet.java
@@ -0,0 +1,19 @@
+package ru.bobby.web;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by bobby on 03.05.2015.
+ */
+public class UserServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("userList.jsp");
+    }
+}
---
+++ src/main/webapp/userList.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bobby
+  Date: 04.05.2015
+  Time: 0:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h2>User List</h2>
+</body>
+</html>
