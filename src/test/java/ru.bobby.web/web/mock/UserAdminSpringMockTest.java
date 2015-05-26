7918f1ba7eee569f4f6dbb86517f390e01582d81
b.istomin <ibobby0386@gmail.com>
2015-05-26 18:05:40 +0300

    spring test

---
+++ src/test/java/ru.bobby.web/web/mock/UserAdminSpringMockTest.java
@@ -0,0 +1,37 @@
+package ru.bobby.web.web.mock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.bobby.web.model.Role;
+import ru.bobby.web.model.User;
+import ru.bobby.web.util.exception.NotFoundException;
+import ru.bobby.web.web.user.AdminUserRestController;
+
+import java.util.EnumSet;
+
+/**
+ * Created by b.istomin on 26.05.2015.
+ */
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserAdminSpringMockTest {
+
+    @Autowired
+    AdminUserRestController controller;
+
+    @Test
+    public void create() {
+        controller.create(new User(5, "sunny", "sunny@com", "555", true, EnumSet.of(Role.ROLE_USER)));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() {
+        controller.delete(0);
+    }
+
+    @Test
+    public void testDelete() {
+        controller.delete(5);
+    }
+}
