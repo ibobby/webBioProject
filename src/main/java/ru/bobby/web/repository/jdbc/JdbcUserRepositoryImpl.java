96d0d19c5c91d7568e396b3d87fe5d0a0b71fb96
b.istomin <ibobby0386@gmail.com>
2015-06-22 14:11:32 +0300

    dao jdbc implementation

--- src/main/java/ru/bobby/web/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/bobby/web/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -1,7 +1,9 @@
 package ru.bobby.web.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import ru.bobby.web.model.User;
@@ -14,6 +16,8 @@ import java.util.List;
  * Created by b.istomin on 16.06.2015.
  */
 public class JdbcUserRepositoryImpl implements UserRepository {
+
+    private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -32,26 +36,42 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public User save(User user) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", user.getId())
+                .addValue("name", user.getName())
+                .addValue("email", user.getEmail())
+                .addValue("password", user.getPassword())
+                .addValue("registered", user.getRegistered())
+                .addValue("enabled", user.isEnabled()
+                );
+        if (user.isNew()) {
+            Number newKey = insertUser.executeAndReturnKey(map);
+            user.setId(newKey.intValue());
+        } else {
+            namedParameterJdbcTemplate.update(
+                    "update users set name=:name, email=:email," +
+                        "password=:password, registered=:registered, enabled=:enabled WHERE id=:id", map);
+        }
+        return user;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
 
     @Override
     public User get(int id) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
     }
 
     @Override
     public User getByEmail(String email) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
     }
 }
