f3a4d81a7e216c5ad40ca384efb4698856189188
b.istomin <ibobby0386@gmail.com>
2015-07-15 09:24:52 +0300

    add LOG, START_SEQ and equals hashcode

--- src/main/java/ru/bobby/web/model/BaseEntity.java
+++ src/main/java/ru/bobby/web/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package ru.bobby.web.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by bobby on 13.05.2015.
  */
 public class BaseEntity {
+
+    protected final static Logger LOG = LoggerFactory.getLogger(BaseEntity.class);
+
+    public static final int START_SEQ = 1000000;
 
     protected Integer id;
 
@@ -30,4 +37,22 @@ public class BaseEntity {
         return (this.id == null);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o.getClass() == getClass())) return false;
+
+        BaseEntity that = (BaseEntity) o;
+        if (id == null || that.id == null) {
+            throw new IllegalStateException("Equals '" + this + "' and '" + that + "' with null id");
+        }
+
+        return id.equals(that.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return (id == null) ? 0 : id;
+    }
 }
