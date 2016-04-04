3d7cbca8746992f740d152712962a09fedd8a011
b.istomin <ibobby0386@gmail.com>
2016-04-04 10:02:09 +0300

    finished AdminUserRestController

--- src/main/java/ru/bobby/bio/web/user/AdminUserRestController.java
+++ src/main/java/ru/bobby/bio/web/user/AdminUserRestController.java
@@ -3,12 +3,15 @@ package ru.bobby.bio.web.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.bobby.bio.model.User;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -24,28 +27,40 @@ public class AdminUserRestController {
     @Autowired
     private UserHelper helper;
 
-    public void create(User user) {
-        helper.create(user);
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User created = helper.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/rest/admin/users/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uriOfNewResource);
+
+        return new ResponseEntity<>(created, httpHeaders, HttpStatus.CREATED);
     }
 
-    public void update(User user) {
+    @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody User user) {
         helper.update(user);
     }
 
-    public void delete(int id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") int id) {
         helper.delete(id);
     }
 
-    public void get(int id) {
-        helper.get(id);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@PathVariable("id") int id) {
+        return helper.get(id);
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> getAll() {
         return helper.getAll();
     }
 
-    public User getByEmail(String email) {
+    @RequestMapping(value = "/by", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User getByEmail(@RequestParam("email") String email) {
         return helper.getByMail(email);
     }
 }
